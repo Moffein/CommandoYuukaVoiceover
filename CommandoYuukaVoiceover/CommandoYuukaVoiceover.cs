cd293d13cdc89282592c07123abd976e25da112a
Moffein <22742925+Moffein@users.noreply.github.com>
2023-04-22 01:07:55 -0700

    Initial Commit

---
+++ CommandoYuukaVoiceover/CommandoYuukaVoiceover.cs
@@ -0,0 +1,126 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using CommandoYuukaVoiceover.Components;
+using R2API;
+using RoR2;
+using RoR2.Audio;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace CommandoYuukaVoiceover
+{
+    [BepInDependency("com.rune580.riskofoptions", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInDependency("com.Alicket.HayaseYuukaCommando")]
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.Schale.CommandoYuukaVoiceover", "CommandoYuukaVoiceover", "1.0.0")]
+    [R2API.Utils.R2APISubmoduleDependency(nameof(SoundAPI), nameof(ContentAddition))]
+    public class CommandoYuukaVoiceover : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> enableVoicelines;
+        private static SurvivorDef commandoSurvivorDef;
+
+        public void Awake()
+        {
+            BaseVoiceoverComponent.Init();
+            RoR2.RoR2Application.onLoad += OnLoad;
+
+            using (var bankStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("CommandoYuukaVoiceover.CommandoYuukaSounds.bnk"))
+            {
+                var bytes = new byte[bankStream.Length];
+                bankStream.Read(bytes, 0, bytes.Length);
+                R2API.SoundAPI.SoundBanks.Add(bytes);
+            }
+
+            CommandoYuukaVoiceoverComponent.nseSpecial = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            CommandoYuukaVoiceoverComponent.nseSpecial.eventName = "Play_CommandoYuuka_CommonSkill";
+            R2API.ContentAddition.AddNetworkSoundEventDef(CommandoYuukaVoiceoverComponent.nseSpecial);
+
+            CommandoYuukaVoiceoverComponent.nseBlock = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            CommandoYuukaVoiceoverComponent.nseBlock.eventName = "Play_CommandoYuuka_Blocked";
+            R2API.ContentAddition.AddNetworkSoundEventDef(CommandoYuukaVoiceoverComponent.nseBlock);
+
+            enableVoicelines = base.Config.Bind<bool>(new ConfigDefinition("Settings", "Enable Voicelines"), true, new ConfigDescription("Enable voicelines when using the Commando Yuuka Skin."));
+            if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.rune580.riskofoptions"))
+            {
+                RiskOfOptionsCompat();
+            }
+
+            commandoSurvivorDef = Addressables.LoadAssetAsync<SurvivorDef>("RoR2/Base/Commando/Commando.asset").WaitForCompletion();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private void RiskOfOptionsCompat()
+        {
+            RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.CheckBoxOption(enableVoicelines));
+        }
+
+        private void AttachVoiceoverComponent(On.RoR2.CharacterBody.orig_Start orig, CharacterBody self)
+        {
+            orig(self);
+            if (self)// && self.bodyIndex == BodyCatalog.FindBodyIndex("CommandoBody")  //Not needed since we can just check skinIndex
+            {
+                if (CommandoYuukaVoiceoverComponent.requiredSkinDefs.Contains(SkinCatalog.GetBodySkinDef(self.bodyIndex, (int)self.skinIndex)))
+                {
+                    BaseVoiceoverComponent existingVoiceoverComponent = self.GetComponent<BaseVoiceoverComponent>();
+                    if (!existingVoiceoverComponent) self.gameObject.AddComponent<CommandoYuukaVoiceoverComponent>();
+                }
+            }
+        }
+
+        private void OnLoad()
+        {
+            bool foundSkin = false;
+            SkinDef[] commandoSkins = SkinCatalog.FindSkinsForBody(BodyCatalog.FindBodyIndex("CommandoBody"));
+            foreach (SkinDef skinDef in commandoSkins)
+            {
+                //nameToken is "ALICKET_SKIN_BAHYCSKINDEFINITION_NAME"
+                if (skinDef.name == "BAHYCSkinDefinition")
+                {
+                    foundSkin = true;
+                    CommandoYuukaVoiceoverComponent.requiredSkinDefs.Add(skinDef);
+                    break;
+                }
+            }
+
+            if (!foundSkin)
+            {
+                Debug.LogError("CommandoYuukaVoiceover: Commando Yuuka SkinDef not found. Voicelines will not work!");
+            }
+            else
+            {
+                On.RoR2.CharacterBody.Start += AttachVoiceoverComponent;
+
+                On.RoR2.SurvivorMannequins.SurvivorMannequinSlotController.RebuildMannequinInstance += (orig, self) =>
+                {
+                    orig(self);
+                    if (self.currentSurvivorDef == commandoSurvivorDef)
+                    {
+                        //Check SkinDef
+                        BodyIndex bodyIndexFromSurvivorIndex = SurvivorCatalog.GetBodyIndexFromSurvivorIndex(self.currentSurvivorDef.survivorIndex);
+                        int skinIndex = (int)self.currentLoadout.bodyLoadoutManager.GetSkinIndex(bodyIndexFromSurvivorIndex);
+                        SkinDef safe = HG.ArrayUtils.GetSafe<SkinDef>(BodyCatalog.GetBodySkins(bodyIndexFromSurvivorIndex), skinIndex);
+                        if (CommandoYuukaVoiceoverComponent.requiredSkinDefs.Contains(safe))
+                        {
+                            bool flag = true;
+                            if (flag)
+                            {
+                                if (Util.CheckRoll(5f))
+                                {
+                                    Util.PlaySound("Play_CommandoYuuka_TitleDrop", self.mannequinInstanceTransform.gameObject);
+                                }
+                                else
+                                {
+                                    Util.PlaySound("Play_CommandoYuuka_Lobby", self.mannequinInstanceTransform.gameObject);
+                                }
+                            }
+                        }
+                    }
+                };
+            }
+
+            CommandoYuukaVoiceoverComponent.ScepterIndex = ItemCatalog.FindItemIndex("ITEM_ANCIENT_SCEPTER");
+        }
+    }
+}
---
+++ CommandoYuukaVoiceover/Components/CommandoYuukaVoiceoverComponent.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using RoR2;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+namespace CommandoYuukaVoiceover.Components
+{
+    public class CommandoYuukaVoiceoverComponent : BaseVoiceoverComponent
+    {
+        public static List<SkinDef> requiredSkinDefs = new List<SkinDef>();
+
+        public static NetworkSoundEventDef nseSpecial;
+        public static NetworkSoundEventDef nseBlock;
+
+        public static ItemIndex ScepterIndex;
+
+        private float lowHealthCooldown = 0f;
+        private float blockedCooldown = 0f;
+        private float specialCooldown = 0f;
+        private float levelCooldown = 0f;
+        private bool acquiredScepter = false;
+
+        protected override void Awake()
+        {
+            spawnVoicelineDelay = 3f;
+            if (Run.instance && Run.instance.stageClearCount == 0)
+            {
+                spawnVoicelineDelay = 6f;
+            }
+            base.Awake();
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+            if (inventory && inventory.GetItemCount(ScepterIndex) > 0) acquiredScepter = true;
+        }
+
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            if (lowHealthCooldown > 0f) lowHealthCooldown -= Time.fixedDeltaTime;
+            if (blockedCooldown > 0f) blockedCooldown -= Time.fixedDeltaTime;
+            if (specialCooldown > 0f) specialCooldown -= Time.fixedDeltaTime;
+        }
+
+        public override void PlayDeath()
+        {
+            TryPlaySound("Play_CommandoYuuka_Defeat", 5f, true);
+        }
+
+        public override void PlayHurt(float percentHPLost)
+        {
+            if (percentHPLost >= 0.15f)
+            {
+                TryPlaySound("Play_CommandoYuuka_TakeDamage", 0f, false);
+            }
+        }
+
+        public override void PlayJump() { }
+
+        public override void PlayLowHealth()
+        {
+            if (lowHealthCooldown > 0f) return;
+            bool playedLowHealth = TryPlaySound("Play_CommandoYuuka_LowHealth", 1.95f, false);
+            if (playedLowHealth) lowHealthCooldown = 60f;
+        }
+
+        public override void PlayPrimaryAuthority() { }
+
+        public override void PlaySecondaryAuthority() { }
+
+        public override void PlaySpawn()
+        {
+            //Use Title Drop in character select instead.
+            /*if (Util.CheckRoll(1f))
+            {
+                TryPlaySound("Play_CommandoYuuka_TitleDrop", 1f, true);
+            }
+            else
+            {
+                TryPlaySound("Play_CommandoYuuka_Spawn", 5f, true);
+            }*/
+            TryPlaySound("Play_CommandoYuuka_Spawn", 5f, true);
+        }
+
+        public override void PlaySpecialAuthority()
+        {
+            if (specialCooldown > 0f) return;
+            //bool played = TryPlaySound("Play_CommandoYuuka_CommonSkill", 1.7f, false);
+            bool played = TryPlayNetworkSound(nseSpecial, 1.7f, false);
+            if (played) specialCooldown = 10f;
+        }
+
+        public override void PlayTeleporterFinish()
+        {
+            TryPlaySound("Play_CommandoYuuka_Victory", 3.8f, false);
+        }
+
+        public override void PlayTeleporterStart()
+        {
+            TryPlaySound("Play_CommandoYuuka_TeleporterStart", 1.95f, false);
+        }
+
+        public override void PlayUtilityAuthority()
+        {
+            TryPlaySound("Play_CommandoYuuka_Shout", 0f, false);
+        }
+
+        public override void PlayDamageBlockedServer()
+        {
+            if (!NetworkServer.active || blockedCooldown > 0f) return;
+            //bool playedBlocked = TryPlaySound("Play_CommandoYuuka_Blocked", 0.75f, false);
+            bool played = TryPlayNetworkSound(nseBlock, 0.75f, false);
+            if (played) blockedCooldown = 10f;
+        }
+
+        public override void PlayLevelUp()
+        {
+            if (levelCooldown > 0f) return;
+            bool played = TryPlaySound("Play_CommandoYuuka_LevelUp", 7f, false);
+            if (played) levelCooldown = 60f;
+        }
+
+        protected override void Inventory_onItemAddedClient(ItemIndex itemIndex)
+        {
+            base.Inventory_onItemAddedClient(itemIndex);
+            if (CommandoYuukaVoiceoverComponent.ScepterIndex != ItemIndex.None && itemIndex == CommandoYuukaVoiceoverComponent.ScepterIndex)
+            {
+                PlayAcquireScepter();
+            } 
+        }
+
+        public void PlayAcquireScepter()
+        {
+            if (acquiredScepter) return;
+            TryPlaySound("Play_CommandoYuuka_AcquireScepter", 3.7f, false);
+            acquiredScepter = true;
+        }
+    }
+}
