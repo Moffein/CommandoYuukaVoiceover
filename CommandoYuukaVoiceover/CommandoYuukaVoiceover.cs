5fb4b6c7b1024a95d8876eafa51d758401bb3462
Moffein <22742925+Moffein@users.noreply.github.com>
2023-07-19 20:25:04 -0700

    Updated version.

--- CommandoYuukaVoiceover/CommandoYuukaVoiceover.cs
+++ CommandoYuukaVoiceover/CommandoYuukaVoiceover.cs
@@ -16,7 +16,7 @@ namespace CommandoYuukaVoiceover
 {
     [BepInDependency("com.rune580.riskofoptions", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency("com.Alicket.HayaseYuukaCommando")]
-    [BepInPlugin("com.Schale.CommandoYuukaVoiceover", "CommandoYuukaVoiceover", "1.2.0")]
+    [BepInPlugin("com.Schale.CommandoYuukaVoiceover", "CommandoYuukaVoiceover", "1.2.2")]
     public class CommandoYuukaVoiceover : BaseUnityPlugin
     {
         public static ConfigEntry<bool> enableVoicelines;
